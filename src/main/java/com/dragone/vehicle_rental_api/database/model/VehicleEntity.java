d5054d4b00324940382d449b68d8f3dc17a14782
Eduardo Dragone <dragone.dev@gmail.com>
2026-08-20 13:44:41 -0300

    feat: add vehicle entity and vehicle status enum

---
+++ src/main/java/com/dragone/vehicle_rental_api/database/model/VehicleEntity.java
@@ -0,0 +1,48 @@
+package com.dragone.vehicle_rental_api.database.model;
+
+import com.dragone.vehicle_rental_api.database.model.enums.VehicleStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "vehicle")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class VehicleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank
+    private String brand;
+
+    @NotBlank
+    private String model;
+
+    @NotNull
+    private Integer year;
+
+    @NotNull
+    @PositiveOrZero
+    private Integer mileage;
+
+    @NotNull
+    @Positive
+    private BigDecimal dailyRate;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private VehicleStatus status;
+
+
+}

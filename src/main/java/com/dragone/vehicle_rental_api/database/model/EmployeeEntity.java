b5f39f5cd2bb3550419d2f51fe75b7d304cd86aa
Eduardo Dragone <dragone.dev@gmail.com>
2026-08-20 13:35:19 -0300

    feat: add employee entity

---
+++ src/main/java/com/dragone/vehicle_rental_api/database/model/EmployeeEntity.java
@@ -0,0 +1,35 @@
+package com.dragone.vehicle_rental_api.database.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Entity
+@Table(name = "employee")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    @NotBlank
+    private String employeeCode;
+    @NotBlank
+    private String name;
+    @Email
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String phone;
+    @NotBlank
+    private String position;
+
+
+}

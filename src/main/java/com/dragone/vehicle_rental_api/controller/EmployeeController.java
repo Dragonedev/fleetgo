a4887c041259b4526e8655b00bebd33abc81925c
Eduardo Dragone <dragone.dev@gmail.com>
2026-08-22 08:55:30 -0300

    fix: update employee controller routes

--- src/main/java/com/dragone/vehicle_rental_api/controller/EmployeeController.java
+++ src/main/java/com/dragone/vehicle_rental_api/controller/EmployeeController.java
@@ -14,7 +14,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/employees")
+@RequestMapping("/v1/employees")
 @RequiredArgsConstructor
 @Validated
 public class EmployeeController {
